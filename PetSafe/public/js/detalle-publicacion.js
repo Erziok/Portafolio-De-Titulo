d4dd873fa36be280e9eab6b3be30e0bc2f87eaf8
razhe <gustavo.acu.rivas@gmail.com>
2022-10-05 13:57:51 -0300

    Design: Detail publications design its done. First version

---
+++ PetSafe/public/js/detalle-publicacion.js
@@ -0,0 +1,21 @@
+//Imagenes preview
+const imgs = document.querySelectorAll('.img-select a'),
+      imgBtns = [...imgs];
+let imgId = 1;
+
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
+});
+
+function slideImage(){
+    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+
+    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+}
+
+window.addEventListener('resize', slideImage);
+//Imagenes preview
