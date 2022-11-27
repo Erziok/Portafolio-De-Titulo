d9c3c140c3a18b904e8d78b8985a8c1f650b8754
razhe <gustavo.acu.rivas@gmail.com>
2022-11-27 18:17:09 -0300

    Dashboard PowerBI updated

--- PetSafe/resources/views/admin/dashboard.blade.php
+++ PetSafe/resources/views/admin/dashboard.blade.php
@@ -2,6 +2,6 @@
 @section('title') Dashboard @endsection
 @section('content')
 <div class="app-body-main-content">
-    <iframe title="Analisis_PetSafe_PBI - Página 1" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMWMxNmQ3MDMtZWE2Mi00NTQ5LTk1NzItMmMyMDYyMTYxYTA0IiwidCI6IjcyZmQwYjVhLThhNmEtNGNmZi04OWY2LWJkZTk2MWY3ZTI1MCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+    <iframe title="Analisis_PetSafe_PBI V_FINAL" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMTk0MGQ1MzAtNGE4ZC00NDFjLWE5NGItNzVmYjA4NDgxZmNhIiwidCI6IjcyZmQwYjVhLThhNmEtNGNmZi04OWY2LWJkZTk2MWY3ZTI1MCIsImMiOjR9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
 </div>
 @endsection
